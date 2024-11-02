d408d7e3369888e15dae6765d27df36d65470957
Lucas Dunhão de Carvalho <ldunhao@hotmail.com>
2024-11-02 20:49:47 -0300

    Update src/components/icons/Linkedin.tsx
    Co-authored-by: Joao-Chagas <67298720+Joao-Chagas@users.noreply.github.com>

--- src/components/icons/Linkedin.tsx
+++ src/components/icons/Linkedin.tsx
@@ -5,8 +5,6 @@ type Props = React.SVGProps<SVGSVGElement>;
 export const LinkedinIcon: React.FC<Props> = ({ className }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="0"
-    height="0"
     viewBox="0 0 50 50"
     className={cn(
       "w-6 h-6 stroke-pink-darklight fill-pink-darklight",
