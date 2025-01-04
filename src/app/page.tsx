5da3f9d15a6a5780618190a27485548ebd306303
ldunhao <ldunhao@hotmail.com>
2025-01-04 20:12:54 -0300

    chore: add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,176 @@
-export default function HomePage() {
+import { Button } from "@/components/Button";
+import { Section } from "@/components/Section";
+import { ChatIcon } from "@/components/icons/Chat";
+import { KeyboardIcon } from "@/components/icons/Keyboard";
+import { SearchNgoIcon } from "@/components/icons/SearchNgo";
+import { NgoCarousel } from "@/components/NgoCarousel";
+
+import Link from "next/link";
+
+import { listNgos } from "@/server/ports/ngo";
+
+export default async function HomePage() {
+  const { data: ngoList } = await listNgos();
+
   return (
-    <div className="p-8 place-items-center">
-      <div>Página de início</div>
+    <div>
+      <Section isPink isCentered>
+        <div className="flex align-items justify-center text-center flex-col">
+          <h1 className="text-xl md:text-6xl font-semibold opacity-70">
+            Conheça instituições pela visão do doador!
+          </h1>
+
+          <p className="text-sm md:text-2xl font-thin opacity-70 text-center w-2/3 self-center mt-3">
+            Deseja fazer uma doação segura mas não sabe para quem? Conhece uma
+            ONG e quer recomendá-la para o mundo? Nós te ajudamos!
+          </p>
+        </div>
+
+        <div className="flex justify-center pt-4">
+          <Link href="/ongs/encontrar" className="w-full">
+            <Button
+              className="self-center"
+              label="Buscar ONG’s"
+              type="button"
+              size="lg"
+            />
+          </Link>
+        </div>
+      </Section>
+
+      <Section isCentered>
+        <div className="flex align-items justify-center text-center flex-col md:pb-10">
+          <h1 className="text-xl md:text-6xl font-semibold text-blue-dark">
+            Como funciona?
+          </h1>
+
+          <p className="text-sm md:text-2xl font-thin opacity-70 text-center w-2/3 self-center mt-5">
+            O nosso objetivo é muito simples: conectar doadores a instituições
+            confiáveis! Para isso, precisamos de você! Olha só como você pode
+            ajudar
+          </p>
+        </div>
+
+        <div className="flex items-center justify-center flex-col md:flex-row md:items-start md:justify-around pt-5">
+          <div className="flex items-center justify-center text-center flex-col md:max-w-72">
+            <KeyboardIcon className="w-20 h-20 flex self-center" />
+
+            <div>
+              <h1 className="text-xl md:text-2xl font-semibold opacity-70">
+                Faça uma pesquisa
+              </h1>
+
+              <p className="text-sm md:text-md font-thin opacity-70 text-center mt-5">
+                Digite o nome ou categoria da ONG no campo de pesquisa e clique
+                em buscar
+              </p>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-center text-center flex-col md:max-w-72 my-5">
+            <SearchNgoIcon className="w-20 h-20 flex self-center" />
+
+            <div>
+              <h1 className="text-xl md:text-2xl font-semibold opacity-70">
+                Encontre uma ONG
+              </h1>
+
+              <p className="text-sm md:text-md font-thin opacity-70 text-center mt-5">
+                Escolha a ONG que quer recomendar
+              </p>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-center text-center flex-col md:max-w-72">
+            <ChatIcon className="w-20 h-20 flex self-center" />
+
+            <div>
+              <h1 className="text-xl md:text-2xl font-semibold opacity-70">
+                Recomende-a
+              </h1>
+            </div>
+          </div>
+        </div>
+      </Section>
+
+      <Section className="min-h-screen lg:flex p-0">
+        <div className="hidden lg:w-1/2 lg:flex lg:bg-pink lg:border-r lg:border-r-pink-dark"></div>
+
+        <div className="w-full min-h-screen lg:w-1/2 flex align-items justify-center text-center flex-col bg-pink-light">
+          <div className="flex align-items justify-center text-center flex-col mb-28">
+            <h1 className="text-xl md:text-6xl font-semibold opacity-70">
+              Sobre a quero doar
+            </h1>
+
+            <p className="text-sm md:text-2xl font-thin opacity-70 text-center w-2/3 self-center mt-3">
+              Nós reunimos depoimentos de pessoas que já foram doadoras ou
+              voluntárias sobre instituições, facilitando o encontro entre novos
+              doadores e instituições confiáveis!
+            </p>
+          </div>
+
+          <div className="flex justify-center mt-10">
+            <Link href="/sobre" className="w-full">
+              <Button
+                className="self-center"
+                label="Saiba mais"
+                type="button"
+                size="lg"
+              />
+            </Link>
+          </div>
+        </div>
+      </Section>
+
+      <Section isCentered>
+        <div className="flex align-items justify-center text-center flex-col md:pb-10">
+          <h1 className="text-xl md:text-6xl font-semibold text-blue-dark">
+            Recomende uma ONG ou cadastre uma!
+          </h1>
+
+          <p className="text-sm md:text-2xl font-thin opacity-70 text-center w-2/3 self-center mt-5">
+            Já conhece alguma ONG e gostaria de recomendá-la? É só clicar em
+            Recomendar. Caso ela não tenha um perfil, você poderá ajudá-la
+            clicando em Cadastrar, e nos dê informações para incluí-la na
+            comunidade.
+          </p>
+        </div>
+
+        <div className="flex items-center justify-center flex-col lg:flex-row mt-10">
+          <Link href="/ongs/recomendar" className="mb-5 lg:mr-10 lg:mb-0">
+            <Button
+              className="self-center"
+              label="Recomendar"
+              type="button"
+              size="lg"
+              variant="outline"
+            />
+          </Link>
+
+          <Link href="/cadastrar">
+            <Button
+              className="self-center"
+              label="Cadastrar"
+              type="button"
+              size="lg"
+            />
+          </Link>
+        </div>
+      </Section>
+
+      <Section isCentered className="bg-gray">
+        <div className="flex align-items justify-center text-center flex-col md:pb-10">
+          <h1 className="text-xl md:text-6xl font-semibold text-blue-dark">
+            ONG’s cadastradas
+          </h1>
+
+          <p className="text-sm md:text-2xl font-thin opacity-70 text-center w-2/3 self-center mt-5">
+            Conheça novas ONG’s
+          </p>
+        </div>
+
+        <NgoCarousel ngos={ngoList!.users} />
+      </Section>
     </div>
   );
 }
